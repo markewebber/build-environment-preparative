cfa55dbaf215b0e1f961a955790e5d41c12cb1e5
Michael S Corigliano (MikeCriggs@XDA) <michael.s.corigliano@gmail.com>
2014-06-10 09:52:07 -0400

    Update run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-# start script
 
-# Created by Michael S Corigliano of Team AOSPAL (michael.s.corigliano@gmail.com)
-
+#
+# Created by Michael S Corigliano for Team AOSPAL (michael.s.corigliano@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # This will run all steps
+#
 
 # 1) Update OS
-   echo -e "Running all steps";
+   echo -e "Running all steps to build Paranoid SaberDroid";
    echo -e "";
    echo -e ""
-   ./1.sh
-   ./2.sh
-   ./3.sh
-
-# end script
-
+   ./step-1.sh
+   ./step-2.sh
+   ./build.sh
