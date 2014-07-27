1a7a42f562e18eeeb5308c9c50e7ee1b0c3430b6
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-07-27 15:36:27 +0300

    install git

--- prepare.sh
+++ prepare.sh
@@ -44,7 +44,7 @@
    libgl1-mesa-dev g++-multilib mingw32 tofrodos \
    python-markdown libxml2-utils xsltproc zlib1g-dev:i386 \
    android-tools-adb android-tools-fastboot libcloog-isl-dev \
-   texinfo curl gcc-multilib libcap-dev libz4* \
+   texinfo curl git gcc-multilib libcap-dev libz4* \
    sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
    clear
    
